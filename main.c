4ecb01f7b64dc6ab70bba6be125ef86a3f50a9ce
GennaroTraino <genny_o_traino@hotmail.it>
2019-06-03 00:59:24 +0200

    first fork

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+
 int main() {
-    printf("Hello World");
+    printf("\nHello World");
+
+
+    if(fork() == 0){
+        printf("\nI'm the son");
+    }
+    else
+    {
+        printf("\nI'm the father");
+    }
+    
     return 0;
 }
